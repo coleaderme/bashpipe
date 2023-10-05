8678b528b4a30f90f7121680c8d6edbebd480fec
coleaderme <coleaderme@gmail.com>
2023-10-05 23:56:49 +0530

    restructured

--- jdl.sh
+++ jdl.sh
@@ -29,15 +29,16 @@ downloader(){
 ## expects token and title as input.
 token=$1
 title=$2
+folder=$3
 input_url="https://www.jiosaavn.com/api.php?__call=webapi.get&token=$token&type=song&includeMetaTags=0&ctx=web6dot0&api_version=4&_format=json&_marker=0"
 echo "[+] [`date +%s`] Downloading/Extracting json"
 ## get encrypted URL
 enc_url=$(xh --pretty=none --ignore-stdin "$input_url" "user-agent:Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0" "cache-control:private, max-age=0, no-cache" | jq .songs[0].more_info.encrypted_media_url)
 echo "[+] [`date +%s`] Decrypting url"
-dl_url=$(python ../pyDes.py "$enc_url")
+dl_url=$(python pyDes.py "$enc_url")
 echo "[+] [`date +%s`] Dowloading"
-wget -q -c -w 1 --random-wait --save-cookies wcookies.txt --keep-session-cookies --header='user-agent:Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0' --header='cache-control:private, max-age=0, no-cache' "$dl_url" -O $title.m4a
-echo "[+] [`date +%s`] Downloaded: $title.m4a"
+wget -q -c -w 1 --random-wait --save-cookies wcookies.txt --keep-session-cookies --header='user-agent:Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0' --header='cache-control:private, max-age=0, no-cache' "$dl_url" -O "$folder/$title.m4a"
+echo "[+] [`date +%s`] Downloaded: '$folder/$title.m4a'"
 }
 
 ################### album/playlist songs #################
@@ -46,7 +47,7 @@ album_dl(){
 echo "[+] Identified: Album"
 album=$(echo "$1" | choose -f '/' -2)
 mkdir "$album"
-cd $album
+# cd $album
 # $1 is album url
 items=$(xh "$1" | htmlq -a href a | rg  "/song")
 max_concurrent=5
@@ -57,7 +58,7 @@ for i in $items; do
     title=$(echo "$i" | choose -f '/' -2)
     token=$(echo "$i" | choose -f '/' -1)
     ## download in parallel //
-    downloader "$token" "$title" &
+    downloader "$token" "$title" "$album" &
     if [ "$bg_counter" -eq "$max_concurrent" ]; then
         wait_with_timeout 10
         bg_counter=0
@@ -71,11 +72,11 @@ wait_with_timeout 10
 track_dl(){
 echo "[+] Identified: Track"
 mkdir singles
-cd singles
+# cd singles
 # $1 is song url
 title=$(echo "$1" | choose -f '/' -2)
 token=$(echo "$1" | choose -f '/' -1)
-downloader "$token" "$title"
+downloader "$token" "$title" "singles"
 }
 
 case "$1" in
